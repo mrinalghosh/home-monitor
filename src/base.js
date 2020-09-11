a1cac4f433466f10dcbb137fb4340b87409055cc
Mrinal Ghosh <ghoshm@bu.edu>
2020-09-11 14:44:38 -0400

    initialized firebase

---
+++ src/base.js
@@ -0,0 +1,16 @@
+import * as firebase from "firebase/app";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDMYhI_pthfVkJjcbFks9yi_-pkB_ywQUw",
+    authDomain: "home-monitor-mg-cg.firebaseapp.com",
+    databaseURL: "https://home-monitor-mg-cg.firebaseio.com",
+    projectId: "home-monitor-mg-cg",
+    storageBucket: "home-monitor-mg-cg.appspot.com",
+    messagingSenderId: "168529580135",
+    appId: "1:168529580135:web:3f20c6ceabdc01dc0a04c2",
+    measurementId: "G-3LJP017V9W"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+
+export default app;
