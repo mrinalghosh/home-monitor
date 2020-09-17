c977e7fba7c3b64d8766c597eff87563c3c2f893
cgerontis <cgeron@bu.edu>
2020-09-16 22:31:15 -0700

    Generated random temp and humidity plots in live feed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,13 +1,69 @@
-import React from 'react'
+import React, {Component} from 'react';
+import {Line} from 'react-chartjs-2';
 
-export default function Feed() {
-    return (
-        <div>
-            <h3>See how humid your house is from anywhere !</h3>
-            <h3>It might be warm too, but now you can know for sure !</h3>
-            {/* plotly.js will be used to create live graphs - 
-            these should be queried with whether a user has 
-            temp/humidity sensors set up and render them*/}
-        </div>
-    )
+// function round(value, decimals) {
+//     return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
+// }
+class Feed extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            tempData:{
+                labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
+                datasets: [{
+                label: 'Temperature("F)',
+                data: [
+                    Math.floor(Math.random() * 40 + 60),
+                    Math.floor(Math.random() * 40 + 60),
+                    Math.floor(Math.random() * 40 + 60),
+                    Math.floor(Math.random() * 40 + 60),
+                    Math.floor(Math.random() * 40 + 60),
+                    Math.floor(Math.random() * 40 + 60)
+                ]
+                ,backgroundColor: 'rgba(255, 99, 132, 0.6)'
+                }
+                ]
+            },
+            humidityData:{
+                labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
+                datasets: [{
+                label: 'Temperature("F)',
+                data: [
+                    Math.floor(Math.random() * 100),
+                    Math.floor(Math.random() * 100),
+                    Math.floor(Math.random() * 100),
+                    Math.floor(Math.random() * 100),
+                    Math.floor(Math.random() * 100),
+                    Math.floor(Math.random() * 100)
+                ]
+                ,backgroundColor: 'rgba(50, 50, 255, 0.6)'
+                }
+                ]
+            }
+    }
 }
+
+    render(){
+        return (
+            <div className="feed" height="150" width="400">
+                <Line
+                    data={this.state.tempData}
+                    options={{
+                        //responsive: true,
+                        maintainAspectRatio: true
+                    }}
+                    />
+                <Line
+                    data={this.state.humidityData}
+                    options={{
+                        // responsive: true,
+                        maintainAspectRatio: true
+                    }}
+                    />
+            </div>
+        )
+    }
+}
+
+export default Feed;
+
--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,47 +1,118 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Line} from 'react-chartjs-2';
 
 // function round(value, decimals) {
 //     return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
 // }
-class Graph extends React.Component {
-    
-constructor(props){
-    super(props);
-    this.state = {
-        chartData:props.chartData
+class Graph extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            graphData:{
+                labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
+                datasets: [{
+                label: 'Population',
+                data: [
+                    617594,
+                    181045,
+                    153060,
+                    106519,
+                    105162,
+                    95072
+                ],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.6)',
+                    'rgba(54, 162, 235, 0.6)',
+                    'rgba(255, 206, 86, 0.6)',
+                    'rgba(75, 192, 192, 0.6)',
+                    'rgba(153, 102, 255, 0.6)',
+                    'rgba(255, 159, 64, 0.6)',
+                    'rgba(255, 99, 132, 0.6)'
+                  ]
+                }
+            ]
+        }
     }
-    }
-
-    static defaultProps = {
-    displayTitle:true,
-    displayLegend: true,
-    legendPosition:'right',
-    location:'City'
-    }
+}
 
     render(){
-    return (
-        <div className="graph">
-    
-        <Line
-            data={this.state.chartData}
-            options={{
-            title:{
-                display:this.props.displayTitle,
-                text:'Largest Cities In '+this.props.location,
-                fontSize:25
-            },
-            legend:{
-                display:this.props.displayLegend,
-                position:this.props.legendPosition
-            }
-            }}
-        />
-
-        </div>
-    )
+        return (
+            <div className="graph">
+                <Line
+                    data={this.state.graphData}
+                    options={{
+                        maintainAspectRatio: false
+                    }}
+                    />
+            </div>
+        )
     }
 }
 
 export default Graph;
+
+
+
+
+
+// let myChart = document.getElementById('myChart').getContext('2d');
+//     Chart.defaults.global.defaultFontFamily = 'Lato';
+//     Chart.defaults.global.defaultFontSize = 18;
+//     Chart.defaults.global.defaultFontColor = '#777';
+
+//     let massPopChart = new Chart(myChart, {
+//       type: 'line', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
+//       data: {
+//         labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
+//         datasets: [{
+//           label: 'Population',
+//           data: [
+//             617594,
+//             181045,
+//             153060,
+//             106519,
+//             105162,
+//             95072
+//           ],
+//           //backgroundColor:'green',
+//           backgroundColor: [
+//             'rgba(255, 99, 132, 0.6)',
+//             'rgba(54, 162, 235, 0.6)',
+//             'rgba(255, 206, 86, 0.6)',
+//             'rgba(75, 192, 192, 0.6)',
+//             'rgba(153, 102, 255, 0.6)',
+//             'rgba(255, 159, 64, 0.6)',
+//             'rgba(255, 99, 132, 0.6)'
+//           ],
+//           borderWidth: 1,
+//           borderColor: '#777',
+//           hoverBorderWidth: 3,
+//           hoverBorderColor: '#000'
+//         }]
+//       },
+//       options: {
+//         title: {
+//           display: true,
+//           text: 'Largest Cities In Massachusetts',
+//           fontSize: 25
+//         },
+//         legend: {
+//           display: true,
+//           position: 'right',
+//           labels: {
+//             fontColor: '#000'
+//           }
+//         },
+//         layout: {
+//           padding: {
+//             left: 50,
+//             right: 0,
+//             bottom: 0,
+//             top: 0
+//           }
+//         },
+//         tooltips: {
+//           enabled: true
+//         }
+//       }
+//     });
