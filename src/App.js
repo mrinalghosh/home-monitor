c72b40fa670497051441848c97f21ba612cfbf2c
Mrinal Ghosh <ghoshm@bu.edu>
2020-09-18 00:30:33 -0400

    navbar fix branch

--- src/App.js
+++ src/App.js
@@ -7,63 +7,17 @@ import {
   About,
   AddDevice,
   FourZeroFour,
-  Weather, 
+  Weather,
   NavigationBar
 } from "./components";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import firebase from "firebase";
-import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
-
-firebase.initializeApp({
-  apiKey: "AIzaSyDMYhI_pthfVkJjcbFks9yi_-pkB_ywQUw",
-  authDomain: "home-monitor-mg-cg.firebaseapp.com"
-})
 
 class App extends Component {
-  state = { isSignedIn: false }
-  uiConfig = {
-    signInFlow: "popup",
-    signInOptions: [
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID
-    ],
-    callbacks: {
-      signInSuccessWithAuthResult: () => false
-    }
-  }
-
-  componentDidMount = () => {
-    firebase.auth().onAuthStateChanged(user => {
-      this.setState({ isSignedIn: !!user })
-      console.log("user", user)
-    })
-  }
-
   render() {
     return (
       <div className="App" >
-        {
-          this.state.isSignedIn ? (
-            <span>
-              <div>Signed In!</div>
-              <button onClick={() => firebase.auth().signOut()}>Sign Out</button>
-              <h2>Signed in as {firebase.auth().currentUser.displayName}</h2>
-              <img alt="profile" src={firebase.auth().currentUser.photoURL} width="5%" />
-            </span>)
-            : (
-              <StyledFirebaseAuth
-                uiConfig={this.uiConfig}
-                firebaseAuth={firebase.auth()}
-              />
-            )
-        }
-
-        <div>
-          <NavigationBar></NavigationBar>
-        </div>
-
         < Router >
-          {/* TODO: 404 popup, email login authentication, graphing in Feed */}
+          <NavigationBar></NavigationBar>
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/login" component={Login} />
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,41 +1,76 @@
-import React, { useState } from 'react';
-import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink
-} from 'reactstrap';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
+import firebase from "firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDMYhI_pthfVkJjcbFks9yi_-pkB_ywQUw",
+    authDomain: "home-monitor-mg-cg.firebaseapp.com"
+})
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
 
 const NavigationBar = (props) => {
-    const [isOpen, setIsOpen] = useState(false);
+    const classes = useStyles();
 
-    const toggle = () => setIsOpen(!isOpen);
+    const [signedIn, setSignedIn] = React.useState(false);
+
+    const uiConfig = {
+        signInFlow: "popup",
+        signInOptions: [
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            firebase.auth.EmailAuthProvider.PROVIDER_ID
+        ],
+        callbacks: {
+            signInSuccessWithAuthResult: () => false
+        }
+    };
+
+    React.useEffect(() => {
+        firebase.auth().onAuthStateChanged(user => {
+            setSignedIn(!!user)
+            console.log("user", user)
+        });
+    }, [signedIn])
 
     return (
-        <div>
-            <Navbar bg="dark" variant="dark">
-                <NavbarBrand href="/">Home Monitor</NavbarBrand>
-                <NavbarToggler onClick={toggle} />
-                <Collapse isOpen={isOpen} navbar>
-                    <Nav className="mr-auto" navbar>
-                        <NavItem>
-                            <NavLink href="/about">About Us</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="/weather">Weather</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="/add">Add Device</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="/feed">Live Feed</NavLink>
-                        </NavItem>
-                    </Nav>
-                </Collapse>
-            </Navbar>
+        <div className={classes.root}>
+            <AppBar position="static" color="transparent">
+                <Toolbar>
+                    <Button color="inherit" component={Link} to="/" >Home Monitor</Button>
+                    <Button color="inherit" component={Link} to="/login" >Login</Button>
+                    <Button color="inherit" component={Link} to="/weather" >Weather</Button>
+                    <Button color="inherit" component={Link} to="/feed" >Feed</Button>
+                    <Button color="inherit" component={Link} to="/add" >Add Device</Button>
+                    {signedIn ? (
+                        <span>
+                            <button onClick={() => firebase.auth().signOut()}>Sign Out</button>
+                        </span>)
+                        : (<StyledFirebaseAuth
+                            uiConfig={uiConfig}
+                            firebaseAuth={firebase.auth()} />)
+                    }
+                    {signedIn ? (
+                        <Button color="inherit" disabled >Signed in as {firebase.auth().currentUser.displayName}</Button>
+                    )
+                        : (<div />)
+                    }
+                </Toolbar>
+            </AppBar>
         </div>
     );
 }
