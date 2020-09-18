6f290db4b5709f487e9fd4ae02620cbd195bf875
Mrinal Ghosh <ghoshm@bu.edu>
2020-09-18 01:01:40 -0400

    multiple line charts

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,69 +1,80 @@
-import React, {Component} from 'react';
-import {Line} from 'react-chartjs-2';
+import React, { Component } from 'react';
+import { Line } from 'react-chartjs-2';
 
-// function round(value, decimals) {
-//     return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
-// }
 class Feed extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            tempData:{
-                labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
+            tempData: {
+                labels: [1, 2, 3, 4, 5, 6],
                 datasets: [{
-                label: 'Temperature("F)',
-                data: [
-                    Math.floor(Math.random() * 40 + 60),
-                    Math.floor(Math.random() * 40 + 60),
-                    Math.floor(Math.random() * 40 + 60),
-                    Math.floor(Math.random() * 40 + 60),
-                    Math.floor(Math.random() * 40 + 60),
-                    Math.floor(Math.random() * 40 + 60)
-                ]
-                ,backgroundColor: 'rgba(255, 99, 132, 0.6)'
+                    label: 'Sensor 1("F)',
+                    data: [
+                        Math.floor(Math.random() * 40 + 60),
+                        Math.floor(Math.random() * 40 + 60),
+                        Math.floor(Math.random() * 40 + 60),
+                        Math.floor(Math.random() * 40 + 60),
+                        Math.floor(Math.random() * 40 + 60),
+                        Math.floor(Math.random() * 40 + 60)
+                    ],
+                    borderWidth: 2,
+                    borderColor: 'red',
+                    fill: false
+                },
+                {
+                    label: 'Sensor 2("F)',
+                    data: [
+                        Math.floor(Math.random() * 40 + 60),
+                        Math.floor(Math.random() * 40 + 60),
+                        Math.floor(Math.random() * 40 + 60),
+                        Math.floor(Math.random() * 40 + 60),
+                        Math.floor(Math.random() * 40 + 60),
+                        Math.floor(Math.random() * 40 + 60)
+                    ],
+                    borderWidth: 2,
+                    borderColor: 'green',
+                    fill: false
                 }
                 ]
             },
-            humidityData:{
-                labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
+            humidityData: {
+                labels: [1, 2, 3, 4, 5, 6],
                 datasets: [{
-                label: 'Temperature("F)',
-                data: [
-                    Math.floor(Math.random() * 100),
-                    Math.floor(Math.random() * 100),
-                    Math.floor(Math.random() * 100),
-                    Math.floor(Math.random() * 100),
-                    Math.floor(Math.random() * 100),
-                    Math.floor(Math.random() * 100)
-                ]
-                ,backgroundColor: 'rgba(50, 50, 255, 0.6)'
+                    label: 'Temperature("F)',
+                    data: [
+                        Math.floor(Math.random() * 100),
+                        Math.floor(Math.random() * 100),
+                        Math.floor(Math.random() * 100),
+                        Math.floor(Math.random() * 100),
+                        Math.floor(Math.random() * 100),
+                        Math.floor(Math.random() * 100)
+                    ]
+                    , borderColor: 'blue',
+                    fill: false
                 }
                 ]
             }
+        }
     }
-}
 
-    render(){
+    render() {
         return (
             <div className="feed" height="150" width="400">
                 <Line
                     data={this.state.tempData}
                     options={{
-                        //responsive: true,
                         maintainAspectRatio: true
                     }}
-                    />
+                />
                 <Line
                     data={this.state.humidityData}
                     options={{
-                        // responsive: true,
                         maintainAspectRatio: true
                     }}
-                    />
+                />
             </div>
         )
     }
 }
 
 export default Feed;
-
